faf552d28d265bb5d382691f8852095b0fb62bf2
Alex Coplan <lexy0202@gmail.com>
2011-07-27 00:27:08 +0100

    Changed Links

--- Countdown/CountdownAppDelegate.h
+++ Countdown/CountdownAppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by Alex Coplan on 26/07/2011 - Questions, or bugs? - Email: lexy0202@gmail.com.
 //  Copyright 2011 Illogical Development. All rights reserved.
 //
-//  This sample code was created in response to a question on Stack Overflow. See the original question here: http://stackoverflow.com/questions/6828807/count-down-timer-in-iphone/
+//  This sample code was created in response to a question on Stack Overflow. See the original question here: http://stackoverflow.com/q/6828807/840973
 
 #import <UIKit/UIKit.h>
 
